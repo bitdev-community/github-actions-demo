0f0f7c342d0ac634157460b0313d08a0dd9ecf97
ashanfernando <ashan256@gmail.com>
2024-08-08 14:21:27 +0000

    Commiting the latest updates from lane: github-actions.feature-branch-demo/test-lane to the Git branch (automated) [skip-ci]

--- demo/components/ui/hello-world/hello-world.spec.tsx
+++ demo/components/ui/hello-world/hello-world.spec.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { BasicHelloWorld } from './hello-world.composition';
 
-it('should render with the correct text', () => {
+it('should render with the correct text "Hello world!"', () => {
   const { getByText } = render(<BasicHelloWorld />);
   const rendered = getByText('Hello world!');
   expect(rendered).toBeTruthy();
